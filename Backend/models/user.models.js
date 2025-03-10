922d7c0fc543f923d535c8ba552eb5251a1a40ad
Abheesht Singh <abheesht.singh2803@gmail.com>
2025-03-10 23:08:01 +0530

    Error Resolve

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        slect: false,
+        select: false,
     },
     socketId: {
         type: String,
